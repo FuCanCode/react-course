36cc9ef5f1b13a26cebb89928d78e7799e797c27
Stefan F. <fustef@outlook.de>
2023-12-18 13:13:40 +0100

    own version of steps component

---
+++ 04-steps/starter/src/Steps.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export default function Steps() {
+  const [step, setStep] = useState(1);
+
+  const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+  ];
+
+  const next = function () {
+    if (step < 3) setStep(step + 1);
+  };
+
+  const previous = function () {
+    if (step > 1) setStep(step - 1);
+  };
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`step-1 ${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`step-2 ${step > 1 ? "active" : ""}`}>2</div>
+        <div className={`step-3 ${step === 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">{`Step: ${step} ${messages[step - 1]}`}</p>
+
+      <div className="buttons">
+        <button
+          className="previous"
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={previous}
+        >
+          Previous
+        </button>
+        <button
+          className="next"
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={next}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
