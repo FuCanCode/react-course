92ad3915547a21e2269c976f6e2003b50e7987fe
Stefan F. <fustef@outlook.de>
2023-12-28 12:39:50 +0100

    lift state up works (but no types)

--- 05-travel-list/starter/src/App.tsx
+++ 05-travel-list/starter/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Form from "./Form";
 import Heading from "./Heading";
 import List from "./List";
@@ -7,10 +8,34 @@ import { items } from "./itemsData";
 export const globalState = new ItemList(items);
 
 function App() {
+  const [item, setItem] = useState("");
+  const [qty, setQty] = useState(1);
+
+  function submitHandler(ev: React.FormEvent<HTMLFormElement>) {
+    ev.preventDefault();
+
+    if (!item) return alert("Please enter a Description!");
+
+    // add item to state
+    globalState.addItem(qty, item);
+
+    // at the end clear input
+    setItem(() => "");
+    setQty(() => 1);
+  }
+
+  function qtyHandler(ev: React.ChangeEvent<HTMLSelectElement>) {
+    setQty(Number(ev.target.value));
+  }
+
+  function itemHandler(ev: React.ChangeEvent<HTMLInputElement>) {
+    setItem(ev.target.value);
+  }
+
   return (
     <>
       <Heading />
-      <Form />
+      <Form {...{ submitHandler, qty, item, qtyHandler, itemHandler }} />
       <List />
     </>
   );
--- 05-travel-list/starter/src/Form.tsx
+++ 05-travel-list/starter/src/Form.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
 import { globalState } from "./App";
 
-export default function Form() {
-  const [item, setItem] = useState("");
-  const [qty, setQty] = useState(1);
-
+export default function Form({
+  submitHandler,
+  qty,
+  item,
+  qtyHandler,
+  itemHandler,
+}) {
   // Select options
   const numOptions = 10;
   const options = [];
@@ -12,24 +14,11 @@ export default function Form() {
     options.push(index + 1);
   }
 
-  function submitHandler(ev: React.FormEvent<HTMLFormElement>) {
-    ev.preventDefault();
-
-    if (!item) return alert("Please enter a Description!");
-
-    // add item to state
-    globalState.addItem(qty, item);
-
-    // at the end clear input
-    setItem(() => "");
-    setQty(() => 1);
-  }
-
   return (
     <form className="add-form" onSubmit={submitHandler}>
       <h3>What do you need for your trip?</h3>
 
-      <select value={qty} onChange={(e) => setQty(Number(e.target.value))}>
+      <select value={qty} onChange={qtyHandler}>
         {options.map((opt) => (
           <option value={opt} key={opt}>
             {opt}
@@ -42,7 +31,7 @@ export default function Form() {
         type="text"
         placeholder="Item..."
         value={item}
-        onChange={(e) => setItem(e.target.value)}
+        onChange={itemHandler}
       />
       <button type="submit">add</button>
     </form>
