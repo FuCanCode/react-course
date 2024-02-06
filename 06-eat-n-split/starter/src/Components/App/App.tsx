e43f8767259217af3e284baa867bd669b9c9b6db
Stefan F. <fustef@outlook.de>
2024-02-06 12:49:23 +0100

    implemented a little summary for debts, credits and balance

--- 06-eat-n-split/starter/src/Components/App/App.tsx
+++ 06-eat-n-split/starter/src/Components/App/App.tsx
@@ -36,6 +36,15 @@ function App() {
   const [selectedPerson, setSelectedPerson] = useState<number | null>(null);
   const [list, setList] = useState<iPeople[]>(initialFriends);
 
+  const debts: number = list.reduce((sum: number, cur: iPeople): number => {
+    return cur.balance < 0 ? sum + cur.balance : sum;
+  }, 0);
+  const credit = list.reduce(
+    (sum, cur) => (cur.balance > 0 ? sum + cur.balance : sum),
+    0
+  );
+  const balance = debts + credit;
+
   function handleAddFriend(name: string, imageURL?: string) {
     const id = Math.round(Math.random() * 99999);
     const newUrl = imageURL ? imageURL : `https://i.pravatar.cc/48?u=${id}`;
@@ -48,8 +57,14 @@ function App() {
     };
 
     setList([...list, newFriend]);
+  }
 
-    console.log(newFriend);
+  function handleSplitBill(updatedDebtor: iPeople) {
+    setList(
+      list.map((debitor) => {
+        return debitor.id === selectedPerson ? updatedDebtor : debitor;
+      })
+    );
   }
 
   return (
@@ -61,9 +76,28 @@ function App() {
           onSelect={setSelectedPerson}
         />
         <AddFriend onAddFriend={handleAddFriend} />
+        <div className="summary">
+          <p>
+            Debts: <span className="red">{debts}€</span>
+          </p>
+          <p>
+            Credits: <span className="green">{credit}€</span>
+          </p>
+          <p>
+            Balance:{" "}
+            <span className={balance > 0 ? "green" : balance < 0 ? "red" : ""}>
+              {balance}€
+            </span>
+          </p>
+        </div>
       </div>
 
-      {selectedPerson && <BillForm />}
+      {selectedPerson && (
+        <BillForm
+          debtor={list.find((debtor) => debtor.id === selectedPerson)!}
+          onSplitBill={handleSplitBill}
+        />
+      )}
     </div>
   );
 }
