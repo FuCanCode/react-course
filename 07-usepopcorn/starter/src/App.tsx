867382bb08a2e0914de31bbaf360c70cac0b758c
Stefan F. <fustef@outlook.de>
2024-03-11 13:03:48 +0100

    add function for fetching search data

--- 07-usepopcorn/starter/src/App.tsx
+++ 07-usepopcorn/starter/src/App.tsx
@@ -1,70 +1,13 @@
 import { ReactNode, useState } from "react";
 import StarRating from "./StarRating";
-
-interface iMovie {
-  imdbID: string;
-  Title: string;
-  Year: string;
-  Poster: string;
-}
-interface iWatchedMovies extends iMovie {
-  runtime: number;
-  imdbRating: number;
-  userRating: number;
-}
-
-interface iSummary {
-  numMovies: number;
-  imdbRating: number;
-  userRating: number;
-  runtime: number;
-}
-const tempMovieData: iMovie[] = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData: iWatchedMovies[] = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
+import {
+  getSearchResults,
+  iMovie,
+  iSummary,
+  iWatchedMovies,
+  tempMovieData,
+  tempWatchedData,
+} from "./lib/data";
 
 const average = (arr: number[]): number => {
   return arr.reduce((acc, cur, _, arr) => acc + cur / arr.length, 0);
@@ -245,7 +188,7 @@ function SearchInput() {
 
   function handlePressEnter(e: React.KeyboardEvent<HTMLInputElement>) {
     if (e.key === "Enter") {
-      // Do something with input
+      getSearchResults(query);
 
       setQuery("");
     }
