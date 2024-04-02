5e4b0719705bab178ef43b645713e28a455454cd
Stefan F. <fustef@outlook.de>
2024-04-02 10:41:03 +0200

    add tailwind styles & input state

--- 09-classy-weather/starter/src/App.tsx
+++ 09-classy-weather/starter/src/App.tsx
@@ -1,17 +1,48 @@
-import Counter from "./Counter";
+import React from "react";
 
-function App() {
-  return (
-    <>
-      <div className="flex flex-col bg-white items-center font-crazy">
-        <h1>Classy Weather</h1>
-        <Counter />
-        <h1>Classy Weather</h1>
-        <h1>Classy Weather</h1>
-        <h1>Classy Weather</h1>
-      </div>
-    </>
-  );
+const containerStyle = [
+  "w-screen",
+  "mx-9",
+  "py-6",
+  "border-solid",
+  "border-2",
+  "border-black",
+  "flex",
+  "flex-col",
+  "gap-3",
+  "items-center",
+  "font-crazy",
+  "outline",
+  "outline-2",
+  "outline-offset-4",
+].join(" ");
+
+class App extends React.Component {
+  state = { input: "" };
+
+  handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ input: e.target.value });
+  };
+
+  render() {
+    return (
+      <>
+        {/* App Container */}
+        <div className={containerStyle}>
+          <h1 className="text-4xl">Classy Weather</h1>
+          <div>
+            <input
+              value={this.state.input}
+              onChange={this.handleInput}
+              className="p-2 bg-red-100"
+              type="text"
+              placeholder="Search from location"
+            />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
