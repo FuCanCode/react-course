1675372d2de81d9925096a3f74ed1c8c6d5e76e7
Stefan F. <fustef@outlook.de>
2024-04-04 13:16:52 +0200

    add spinner and component for each day

--- 09-classy-weather/starter/src/App.tsx
+++ 09-classy-weather/starter/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { getWeather } from "./weatherData";
+import { Bars } from "react-loader-spinner";
 
 interface IWeather {
   name: string;
@@ -19,6 +20,7 @@ interface IAppState {
   input: string;
   error: string;
   weather: IWeather | null;
+  isLoading: boolean;
 }
 
 const containerStyle = [
@@ -39,25 +41,36 @@ const containerStyle = [
 ].join(" ");
 
 class App extends React.Component<unknown, IAppState> {
-  state: IAppState = { input: "Dresden", error: "", weather: null };
+  state: IAppState = {
+    input: "Dresden",
+    error: "",
+    weather: null,
+    isLoading: false,
+  };
 
   handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     this.setState({ input: e.target.value });
   };
 
   handleButtonClick = async () => {
-    this.setState({ input: "" });
+    this.setState(() => ({ isLoading: true }));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     const result = await getWeather(this.state.input);
+
     if (!result) return;
     if (typeof result === "string") {
-      this.setState({ ...this.state, error: result });
+      this.setState((prev) => ({ ...prev, error: result, isLoading: false }));
     } else {
-      this.setState({ ...this.state, weather: { ...result } });
+      this.setState((prev) => ({
+        ...prev,
+        weather: { ...result },
+        isLoading: false,
+      }));
     }
   };
 
   render() {
-    const { weather } = this.state;
+    const { weather, isLoading } = this.state;
 
     return (
       <>
@@ -68,17 +81,22 @@ class App extends React.Component<unknown, IAppState> {
             <input
               value={this.state.input}
               onChange={this.handleInput}
-              className="p-2 bg-red-100"
+              className="p-2 bg-red-100 text-center"
               type="text"
               placeholder="Search from location"
+              disabled={isLoading}
             />
           </div>
-          <button
-            className="border border-solid border-black rounded-md text-lg py-2 px-3"
-            onClick={this.handleButtonClick}
-          >
-            Get Weather
-          </button>
+          {!isLoading ? (
+            <button
+              className="border border-solid border-black rounded-md text-lg py-2 px-3"
+              onClick={this.handleButtonClick}
+            >
+              Get Weather
+            </button>
+          ) : (
+            <Bars color="black" visible={isLoading} />
+          )}
           {weather && (
             <>
               <h2>
@@ -106,17 +124,45 @@ class SevenDays extends React.Component<ISevenDays> {
     return (
       <ul className="flex flex-col sm:flex-row gap-3 font-sans">
         {weekdays.map((weekday, i) => (
-          <li
+          <OneDay
             key={i}
-            className="w-20 bg-white flex flex-col items-center gap-2 p-2"
-          >
-            <div>{weekday}</div>
-            <div>{icons[i]}</div>
-            <div>Max {maxTemps[i]}</div>
-            <div>Min {minTemps[i]}</div>
-          </li>
+            day={i === 0 ? "Today" : weekday}
+            icon={icons[i]}
+            max={maxTemps[i]}
+            min={minTemps[i]}
+          />
         ))}
       </ul>
+    );
+  }
+}
+
+interface OneDayProps {
+  day: string;
+  icon: string;
+  max: number;
+  min: number;
+}
+
+class OneDay extends React.Component<OneDayProps> {
+  constructor(props: OneDayProps) {
+    super(props);
+  }
+
+  render() {
+    const { day, icon, max, min } = this.props;
+
+    return (
+      <li className="w-24 bg-white flex flex-col items-center gap-2 p-2">
+        <div>{day}</div>
+        <div className="text-3xl">{icon}</div>
+        <div className="font-light">
+          Max <span className="text-red-400">{max}&deg;</span>
+        </div>
+        <div className="font-light">
+          Min <span className="text-blue-400">{min}&deg;</span>
+        </div>
+      </li>
     );
   }
 }
