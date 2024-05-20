054c8fc5e0b6603845cf6cc51222656453ac2b36
Stefan F. <fustef@outlook.de>
2024-05-20 10:31:33 +0200

    add some stylings for result component

--- 10-react-quiz/starter/src/ui/Result.tsx
+++ 10-react-quiz/starter/src/ui/Result.tsx
@@ -8,11 +8,11 @@ function Result(props: ResultProps) {
   const [curPoints, maxPoints] = props.points;
   const percent = Math.round((curPoints / maxPoints) * 100);
   return (
-    <div>
+    <div className="summary">
       <h2>{props.timeOver ? "Time is over!" : "All questions completed!"}</h2>
-      <h2>
+      <h3>
         You've finished with {curPoints} out of {maxPoints} ({percent}%) points!
-      </h2>
+      </h3>
       <button className="btn" onClick={() => props.onRestart()}>
         Restart
       </button>
