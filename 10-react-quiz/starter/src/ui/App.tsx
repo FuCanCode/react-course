02e11cdf39fc0493e169843dc44053c2536381a3
Stefan F. <fustef@outlook.de>
2024-06-06 10:54:05 +0200

    remove custom hook to be on par with the course; add Error and Loader cmp

--- 10-react-quiz/starter/src/ui/App.tsx
+++ 10-react-quiz/starter/src/ui/App.tsx
@@ -1,24 +1,40 @@
 import Header from "./Header";
 import Main from "./Main";
-import useFakeApi from "../../data/useFakeApi";
-import { quizReducer } from "../../data/quizReducer";
-import { useReducer } from "react";
+//import useFakeApi from "../../data/useFakeApi";
+import { QUIZ_URL } from "../../data/useFakeApi";
+import { QuizItem, quizReducer } from "../../data/quizReducer";
+import { useEffect, useReducer } from "react";
 import Quiz from "./Quiz";
 import { ProgressProps } from "./Progress";
 import QuizIntro from "./QuizIntro";
 import { initQuizState } from "../initQuizState";
+import Loader from "./Loader";
+import Error from "./Error";
 
 function App() {
   const [quizState, dispatch] = useReducer(quizReducer, initQuizState);
+  const { currentQuestion, points, status, timeLeft, questions } = quizState;
+  //const quizItems = useFakeApi();
+  useEffect(() => {
+    async function getQuestions() {
+      try {
+        const res = await fetch(QUIZ_URL);
+        const questions: QuizItem[] = await res.json();
+        dispatch({ type: "setQuestions", questions: questions });
+      } catch (error) {
+        console.log(error);
+        dispatch({ type: "error" });
+      }
+    }
 
-  const quizItems = useFakeApi();
+    setTimeout(getQuestions, 2000);
+  }, []);
 
-  const { currentQuestion, points, isStarted, timeLeft } = quizState;
   const progress: ProgressProps = {
     curQuestion: currentQuestion + 1,
-    maxQuestions: quizItems.length,
+    maxQuestions: questions.length,
     curPoints: points,
-    maxPoints: quizItems.reduce((sum, question) => sum + question.points, 0),
+    maxPoints: questions.reduce((sum, question) => sum + question.points, 0),
   };
 
   return (
@@ -26,16 +42,23 @@ function App() {
       <div className="app">
         <Header />
         <Main>
-          {!isStarted ? (
-            <QuizIntro action={() => dispatch({ type: "startGame" })} />
-          ) : (
+          {status === "loading" ? <Loader /> : null}
+          {status === "error" ? <Error /> : null}
+          {status === "ready" ? (
+            <QuizIntro
+              numberOfQuestions={progress.maxQuestions}
+              action={() => dispatch({ type: "startGame" })}
+            />
+          ) : null}
+
+          {status === "active" ? (
             <Quiz
-              quizItem={quizItems[currentQuestion]}
+              quizItem={questions[currentQuestion]}
               progress={progress}
               actions={dispatch}
               timeLeft={timeLeft}
             />
-          )}
+          ) : null}
         </Main>
       </div>
     </>
