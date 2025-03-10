c53f058f9139f8a94b7c2e78da7a47201f7a903f
Cody McFoo <132007065+FuCanCode@users.noreply.github.com>
2025-03-10 10:32:04 +0000

    330. Global Styles With Styled Components

--- 17-the-wild-oasis/the-wild-oasis/src/App.tsx
+++ 17-the-wild-oasis/the-wild-oasis/src/App.tsx
@@ -1,7 +1,29 @@
+import styled from "styled-components";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
   return (
     <>
-      <div>Blub</div>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>A Styled Heading</H1>
+        <Button>Check in</Button>
+        <Button>Check out</Button>
+        <Input placeholder="Number of guests" type="number" />
+        <div>Blub</div>
+      </StyledApp>
     </>
   );
 }
